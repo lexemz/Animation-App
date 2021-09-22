ee763ecf0b36d50aec3e209f972a5c97eba66c6d
Igor Buzykin <lexemz@gmail.com>
2021-09-22 16:30:23 +0300

    Work is done

--- Spring Animation App/Models/Animation Model.swift
+++ Spring Animation App/Models/Animation Model.swift
@@ -5,4 +5,20 @@
 //  Created by Igor on 22.09.2021.
 //
 
-import Foundation
+
+struct Animation {
+    let animation: String
+    let duration: Double
+    let curve: String
+    var force: Double = 1.0
+    var delay: Double = 0
+    
+    static func getAnimations() -> [Animation] {
+        [
+            Animation(animation: "morph", duration: 1, curve: "easeOut", force: 1.0, delay: 0),
+            Animation(animation: "pop", duration: 1, curve: "easeOut"),
+            Animation(animation: "swing", duration: 0.7, curve: "spring", force: 1.5, delay: 0.1),
+            Animation(animation: "wobble", duration: 1.1, curve: "spring", force: 0.5, delay: 0.2),
+        ]
+    }
+}
--- Spring Animation App/ViewControllers/ViewController.swift
+++ Spring Animation App/ViewControllers/ViewController.swift
@@ -5,15 +5,92 @@
 //  Created by Igor on 21.09.2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
+    // MARK: - IBOutlets
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var animatedView: SpringView!
+    @IBOutlet var animationInfoLabel: UILabel! {
+        didSet {
+            animationInfoLabel.textColor = .white
+        }
     }
 
+    @IBOutlet var runAnimateButton: SpringButton!
+    
+    // MARK: - Private properties
 
+    private var animations = Animation.getAnimations()
+    private var animationCount = 0
+    
+    // MARK: - Life cycle methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpAnimationsSet()
+    }
+
+    // MARK: - IBActions
+
+    @IBAction func runAnimateButtonPressed() {
+        animateButton()
+        animateView()
+    }
+    
+    // MARK: - Private methods
+    private func animateView() {
+        let currentAnimation = animations[animationCount]
+        
+        animatedView.animation = currentAnimation.animation
+        animatedView.duration = currentAnimation.duration
+        animatedView.curve = currentAnimation.curve
+        animatedView.force = currentAnimation.force
+        animatedView.delay = currentAnimation.delay
+
+        animatedView.animate()
+        
+        guard animations.count - 1 != animationCount else {
+            setUpAnimationsSet()
+            return
+        }
+        animationCount += 1
+        setAnimationInfo(animation: animations[animationCount])
+        
+        guard animations[animationCount].animation != animations.last?.animation else {
+            setButtonInfo(title: "Restart \(animations.first?.animation ?? "")")
+            return
+        }
+        setButtonInfo(title: "Next \(animations[animationCount + 1].animation)")
+    }
+    
+    private func animateButton() {
+        runAnimateButton.animation = "fadeIn"
+        runAnimateButton.curve = "spring"
+        runAnimateButton.duration = 1.0
+        
+        runAnimateButton.animate()
+    }
+    
+    private func setUpAnimationsSet() {
+        animationCount = 0
+        
+        setAnimationInfo(animation: animations[animationCount])
+        setButtonInfo(title: "Next \(animations[animationCount + 1].animation)")
+    }
+    
+    private func setAnimationInfo(animation: Animation) {
+        animationInfoLabel.text = """
+        Animation: \(animation.animation)
+        Duration: \(animation.duration)
+        Curve: \(animation.curve)
+        Force: \(animation.force)
+        Delay: \(animation.delay)
+        """
+    }
+    
+    private func setButtonInfo(title: String) {
+        runAnimateButton.setTitle(title, for: .normal)
+    }
 }
-
